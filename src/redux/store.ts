27223fca6d8c383f68a0bd7da2331c749752201b
Artyom Chernykh <artemon3162@gmail.com>
2021-03-31 11:10:47 +0300

    add redux devtools chrome

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,13 @@
 import {combineReducers, createStore, applyMiddleware} from "redux";
 import itemsReducer from './items-reducer';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 let reducers = combineReducers({
   items: itemsReducer
 })
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
